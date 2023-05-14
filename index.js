5c49ded187fb13d7cc26d8afd67378a398983bfc
moezkh <khorchani_moez_eddine@outlook.com>
2023-05-14 15:27:35 +0100

    fix server

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ require('dotenv').config()
 const app = express();
 
 app.use(cors());
- 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/api/autosuggest/:at', async function (req, res) {
 
     try {
